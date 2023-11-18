60c24d11a49c030e212193ed85eb330d56407185
sumitjambharkar <sujam3545@gmail.com>
2023-11-18 18:21:45 +0530

    commit

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,25 +1,76 @@
-import Link from 'next/link'
-import React from 'react';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Navbar = () => {
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShow(window.innerWidth < 512);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <section className='navbar'>
-      <div className='child_link'>
-      <h1><Link className="navbar-brand" href="/">Clean<span>Nation</span></Link></h1>
+    <section className="navbar">
+      <div className="child_link_logo">
+        <h1>
+          <Link className="navbar-brand" href="/">
+            Clean<span>Nation</span>
+          </Link>
+        </h1>
+        {show ? (
+          <MenuIcon
+            onClick={() => setShow(false)}
+            className="menu"
+            fontSize="large"
+          />
+        ) : (
+          <CloseIcon
+            onClick={() => setShow(true)}
+            className="menu"
+            fontSize="large"
+          />
+        )}
       </div>
-      <div className='child_link'>
-       <li><Link href="/">Home</Link></li>
-       <li><Link href="/about">About</Link></li>
-       <li><Link href="/service">Service</Link></li>
-       <li><Link href="/contact">Contact</Link></li>
-       <div className="read">
-							<Link href="/login" className="btn">Sign Up</Link>
-						</div>
-       {/* <li><Link href="/"><ShoppingCartIcon/></Link></li> */}
+      <div className={!show ? "child_link" : "child_link_hide"}>
+        <li>
+          <Link onClick={() => setShow(true)} href="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link onClick={() => setShow(true)} href="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link onClick={() => setShow(true)} href="/service">
+            Service
+          </Link>
+        </li>
+        <li>
+          <Link onClick={() => setShow(true)} href="/contact">
+            Contact
+          </Link>
+        </li>
+        <div className="read">
+          <Link onClick={() => setShow(true)} href="/login" className="btn">
+            Sign Up
+          </Link>
+        </div>
+        {/* <li><Link href="/"><ShoppingCartIcon/></Link></li> */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
