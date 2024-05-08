cdd30e6e612b5fc31b1e9d1f568fad66a203df4d
sssjambharkar <sumit@kdigitalcurry.com>
2024-05-08 20:08:30 +0530

    commit

---
+++ src/app/sitemap.js
@@ -0,0 +1,28 @@
+export default async function sitemap() {
+
+
+    return [{
+        
+        url:"https://cleannation.in",
+        lastModified:new Date(),
+    },{
+        
+        url:"https://cleannation.in/about",
+        lastModified:new Date(),
+    },{
+        
+        url:"https://cleannation.in/blog",
+        lastModified:new Date(),
+    }
+    ,{
+        
+        url:"https://cleannation.in/service",
+        lastModified:new Date(),
+    }
+    ,{
+        
+        url:"https://cleannation.in/contact",
+        lastModified:new Date(),
+    }
+]
+}
