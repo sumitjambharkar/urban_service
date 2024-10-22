28d05789396cfabf487d36c1422218c7bc2fe192
sumitjambharkar <sujam3545@gmail.com>
2024-10-22 21:36:26 +0530

    comm

--- src/app/components/Slider.js
+++ src/app/components/Slider.js
@@ -7,7 +7,7 @@ const Slider = () => {
         <h6>Fast and efficient</h6>
         <h5>Quality service for quality home</h5>
         <div className="read">
-							<Link className="btn mt-4" href="services.html">Get Quote</Link>
+							<Link className="btn mt-4" href="/service">Get Quote</Link>
 				</div>
       </div>
     </div>
