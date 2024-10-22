4db979c4b83de0b75dc45762b6a6ae75763db04b
sumitjambharkar <sujam3545@gmail.com>
2024-10-22 21:22:10 +0530

    comm

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Top Chandelier Cleaning & Home Cleaning Services",
+  title: "Best Chandelier Cleaning Services Near Me |  7021595850 ✔️",
   description: "CleanNation Company Is A Professional All Service Provide In All Over India With low Cost.The Customer Is The King And We Are The Servant",
 };
 
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <meta name="google-site-verification" content="H51ghbt3Cv1komaELnRrkuUxrigBjWq8AQIqmDs_xZ4" />
 
       <GoogleAnalytics gaId="G-HPXHH3B1BZ" />
 
